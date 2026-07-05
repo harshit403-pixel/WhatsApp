c59a43063982832c655c00035c68094bc401587e
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-07-05 15:12:47 +0530

    feat: add socket authentication middleware to verify user tokens

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,9 +1,26 @@
 import {Server} from 'socket.io'
+import { verifyAccessToken } from '../utils/auth.util.js'
 
 
 export function initializeSocketServer(httpServer) {
 
     const io = new Server(httpServer)
+
+    io.use((socket, next)=>{
+
+        const token = socket.handshake.headers.authorization?.split(" ")[ 1 ]
+
+        if(!token){
+            return next(new Error("Authentication error:No token Provided"))
+        }
+        try {
+            const decoded = verifyAccessToken(token)
+            socket.userId = decoded.userId
+            next()
+        } catch (error) {
+            return next(new Error ("authenticaiton error : invalid token"))
+        }
+    })
 
     io.on("connection", (socket)=>{
         console.log("A user connected:", socket.id)
